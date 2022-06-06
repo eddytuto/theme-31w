f940ce5258c2d4025ae45ec59b73d440221adb93
Eddy Martin <eddypmartin@gmail.com>
2022-06-06 17:07:51 -0400

    s4-c6 ajustement de l'étiquette de version de style.css avec filemtine()

--- index.php
+++ index.php
@@ -3,7 +3,16 @@
 <?php
 if (have_posts()):
     while(have_posts()) : the_post(); ?>
-        <p><?php the_title(); ?></p>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content(); ?></p>
+
+
+<?php $lien = "<a href='" . get_permalink()  . "'>lien</a>"; 
+echo $lien;
+?>
+<p><?= wp_trim_words(get_the_content(), 20, "") ?></p>
+
+
     <?php endwhile; ?>
 <?php endif; ?>    
 
