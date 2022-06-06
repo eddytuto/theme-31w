ca03173332c5510f84a71542fb4d10fccf39c361
Eddy Martin <eddypmartin@gmail.com>
2022-06-06 17:59:56 -0400

    s4-c6 ajout github-page

--- index.php
+++ index.php
@@ -1,19 +1,9 @@
 <?php get_header(); ?> 
-<h1>Mon premier modèle Wordpress</h1>
+<h1>index.php Mon premier modèle Wordpress</h1>
 <?php
 if (have_posts()):
     while(have_posts()) : the_post(); ?>
         <h2><?php the_title(); ?></h2>
-        <p><?php the_content(); ?></p>
-
-
-<?php $lien = "<a href='" . get_permalink()  . "'>lien</a>"; 
-echo $lien;
-?>
-<p><?= wp_trim_words(get_the_content(), 20, "") ?></p>
-
-
     <?php endwhile; ?>
 <?php endif; ?>    
-
 <?php get_footer(); ?>
